ccf7549625b7c18b79695599b6068a6ba7fb3eab
rohitrajak2004 <rajakrohit387@gmail.com>
2026-01-21 13:48:34 +0530

    feat: update title and enhance Navbar with responsive links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,44 @@ import { useState } from "react";
 const Navbar = () => {
   const [isOpen, setisOpen] = useState(true);
   return (
-    <div className="p-5 bg-[#F9F9F9] shadow-md w-screen overflow-x-hidden">
+    <div className="p-5 bg-[#F9F9F9] shadow-md w-screen overflow-x-hidden md:px-15 lg:px-15">
       <div className="flex items-center justify-between">
-        <h1 className="font-pacifico italic font-extrabold text-3xl tracking-tighter">
+        <h1 className="font-pacifico italic font-extrabold text-3xl tracking-tighter w-fit">
           Tastebite
         </h1>
+        <div className="hidden md:flex md:gap-13 lg:flex lg:gap-15 ">
+          <NavLink
+            onClick={() => setisOpen(true)}
+            to="/"
+            className="font-inter md:font-medium lg:font-medium text-lg font-medium hover:text-[#ff642f]"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            onClick={() => setisOpen(true)}
+            to="/recipes"
+            className="font-inter text-lg font-medium hover:text-[#ff642f] md:font-medium lg:font-medium"
+          >
+            Recipes
+          </NavLink>
+          <NavLink
+            onClick={() => setisOpen(true)}
+            to="/create-recipe"
+            className="font-inter text-lg font-medium hover:text-[#ff642f] md:font-medium lg:font-medium"
+          >
+            Add Recipe
+          </NavLink>
+          <NavLink
+            onClick={() => setisOpen(true)}
+            to="/about"
+            className="font-inter text-lg font-medium hover:text-[#ff642f] md:font-medium lg:font-medium"
+          >
+            About
+          </NavLink>
+          
+        </div>
         <button
+          className="md:hidden lg:hidden"
           onClick={() => {
             isOpen ? setisOpen(false) : setisOpen(true);
           }}
@@ -24,36 +56,36 @@ const Navbar = () => {
       </div>
 
       <div
-        className={`overflow-hidden origin-top transform transition-all duration-300 ease-in-out ${isOpen ? "scale-y-0 max-h-0 opacity-0" : " pt-5 flex flex-col gap-5 opacity-100 max-h-52 scale-y-100"}`}
+        className={`md:hidden lg:hidden overflow-hidden origin-top transform transition-all duration-300 ease-in-out ${isOpen ? "scale-y-0 max-h-0 opacity-0" : " pt-5 flex flex-col gap-5 opacity-100 max-h-52 scale-y-100"}`}
       >
         <hr className={`${isOpen ? "hidden" : "block"}`} />
         <NavLink
           onClick={() => setisOpen(true)}
           to="/"
-          className="font-inter text-lg font-medium hover:text-gray-600"
+          className="font-inter text-lg font-medium hover:text-[#ff642f]"
         >
           Home
         </NavLink>
         <NavLink
           onClick={() => setisOpen(true)}
           to="/recipes"
-          className="font-inter text-lg font-medium hover:text-gray-600"
+          className="font-inter text-lg font-medium hover:text-[#ff642f]"
         >
           Recipes
         </NavLink>
         <NavLink
           onClick={() => setisOpen(true)}
-          to="/about"
-          className="font-inter text-lg font-medium hover:text-gray-600"
+          to="/create-recipe"
+          className="font-inter text-lg font-medium hover:text-[#ff642f]"
         >
-          About
+          Add Recipe
         </NavLink>
         <NavLink
           onClick={() => setisOpen(true)}
-          to="/create-recipe"
-          className="font-inter text-lg font-medium hover:text-gray-600"
+          to="/about"
+          className="font-inter text-lg font-medium hover:text-[#ff642f]"
         >
-          Create Recipe
+          About
         </NavLink>
       </div>
     </div>
